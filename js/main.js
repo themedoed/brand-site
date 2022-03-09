edfeff6487b100fe67f61509186287a91bc9c871
themedoed <93190084+themedoed@users.noreply.github.com>
2022-03-09 18:25:46 +0700

    Done Without Adtive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
-$(function(){
-
+$(function () {
+  $(".portfolio__slider").slick({
+  infinite: true,
+  // autoplay: true,
+  autoplaySpeed: 10000,
+  slidesToShow: 1,
+  nextArrow: '<button type="button" class="slick-next"><img src="images/portfolio/slider-arrow.svg" alt="Right-Arrow"></button>',
+  prevArrow: ''
+  });
 });
 
-
-
-const anchors = document.querySelectorAll('.menu-item a[href*="#"]')
+const anchors = document.querySelectorAll('.nav-link[href*="#"]');
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-  e.preventDefault()
-   
-  const blockID = anchor.getAttribute('href').substr(1)
-   
-  document.getElementById(blockID).scrollIntoView({
-  behavior: 'smooth',
-  block: 'start'
-  })
-  })
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 }
