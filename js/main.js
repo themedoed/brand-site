7d37aad1de0b30274d8ef040938763cdab092f53
themedoed <93190084+themedoed@users.noreply.github.com>
2022-03-24 18:10:22 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Настройки слайдера
 $(function () {
   $(".portfolio__slider").slick({
     infinite: true,
@@ -8,16 +9,54 @@ $(function () {
     // nextArrow: '<button type="button" class="slick-next"><img src="images/portfolio/slider-arrow.svg" alt="Right-Arrow"></button>',
     // prevArrow: ''
   });
+});
 
-  $(".portfolio__sldier-arrow-btn").on("click", function () {
-    $(".portfolio__slider").slick("slickNext");
-  });
+// Кнопка переключения слайдов
+$(".portfolio__sldier-arrow-btn").on("click", function () {
+  $(".portfolio__slider").slick("slickNext");
+});
 
+// Адаптив
+if ( $(window).width() < 780 ) {
+  // Меню
   $(".menu__btn").on("click", function () {
     $(".menu__inner").slideToggle();
   });
-});
+  
+  
+  // Аккордеон
+  $(".features-slide-btn-1").on("click", function () {
+    $(".whoweare__features-text-slide-1").slideToggle();
 
+    if (!$(".features-arrow-img-1").hasClass("active")) {
+      $(".features-arrow-img-1")
+        .addClass("active")
+        .attr("src", "images/whoweare/whoweare-arrow-up-img.svg");
+    } else $(".features-arrow-img-1").removeClass("active").attr("src", "images/whoweare/whoweare-arrow-down-img.svg");
+  });
+
+  $(".features-slide-btn-2").on("click", function () {
+    $(".whoweare__features-text-slide-2").slideToggle();
+
+    if (!$(".features-arrow-img-2").hasClass("active")) {
+      $(".features-arrow-img-2")
+        .addClass("active")
+        .attr("src", "images/whoweare/whoweare-arrow-up-img.svg");
+    } else $(".features-arrow-img-2").removeClass("active").attr("src", "images/whoweare/whoweare-arrow-down-img.svg");
+  });
+
+  $(".features-slide-btn-3").on("click", function () {
+    $(".whoweare__features-text-slide-3").slideToggle();
+
+    if (!$(".features-arrow-img-3").hasClass("active")) {
+      $(".features-arrow-img-3")
+        .addClass("active")
+        .attr("src", "images/whoweare/whoweare-arrow-up-img.svg");
+    } else $(".features-arrow-img-3").removeClass("active").attr("src", "images/whoweare/whoweare-arrow-down-img.svg");
+  });
+}
+
+// Плавные якоря
 const anchors = document.querySelectorAll('.nav-link[href*="#"]');
 
 for (let anchor of anchors) {
